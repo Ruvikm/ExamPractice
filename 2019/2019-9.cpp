a56b1ccad6fc407b58a30061c46a84fa89279a97
ruvik <myxrwq@126.com>
2022-06-21 20:44:11 +0800

    这次应该就好了

--- 2019/2019-9.cpp
+++ 2019/2019-9.cpp
@@ -14,31 +14,31 @@ class CirQueue{
 
 void CirQueue :: Create(){
     int data[QueueSize] = {-1};
-    rear = 0, front = QueueSize - 1;
+    rear = front = 0;
     flag = 0;
 }
 
 void CirQueue :: EnQueue(int x){
-    if(rear == front){
+    if(rear == front && flag == 1){
         cout << "队列已满!" << endl;
     }
     else{
         data[rear] = x;
-        // cout << "rear:" << rear << endl;
+        // cout << "rear:" << rear <<"  x :"<< x << endl;
         rear = (rear + 1) % QueueSize;
-        // if(rear == front)
-        //     flag = 1;
+        if(rear == front)
+            flag = 1;
     }
 }
 
 int CirQueue :: DeQueue(){
-    // cout << "front:" <<front <<endl;
-    if((front + 1) % QueueSize == rear && flag == 0){
+    if(front == rear && flag == 0){
         cout << "队列已空!" << endl;
         return -1;
     }else{
         front = (front + 1) % QueueSize;
         int num = data[front];
+        // cout << "front:" << front <<"  num :"<< num << endl;
         if(front == rear)
             flag = 0;
         return num;
@@ -53,7 +53,7 @@ int main(){
     for(int i = 1 ; i <= 5 ; i++){
         Queue.EnQueue(i);
     }
-    for(int i = 1 ; i <= 10 ; i++){
+    for(int i = 1 ; i <= 5 ; i++){
         cout << Queue.DeQueue() << " ";
     }
     cout << endl;
